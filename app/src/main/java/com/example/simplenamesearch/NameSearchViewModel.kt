834d47c5ce12bbd281aaf67fbef5aa45adf6d9a7
jk-truong <41808114+jk-truong@users.noreply.github.com>
2022-10-21 14:19:40 -0700

    Update Coroutine method

--- app/src/main/java/com/example/simplenamesearch/NameSearchViewModel.kt
+++ app/src/main/java/com/example/simplenamesearch/NameSearchViewModel.kt
@@ -2,13 +2,12 @@ package com.example.simplenamesearch
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import io.ktor.client.*
 import io.ktor.client.engine.cio.*
 import io.ktor.client.features.json.*
 import io.ktor.client.request.*
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.async
-import kotlinx.coroutines.withContext
+import kotlinx.coroutines.*
 
 private const val AGE_ENDPOINT = "https://api.agify.io/?name="
 private const val GENDER_ENDPOINT = "https://api.genderize.io/?name="
@@ -26,30 +25,32 @@ class NameSearchViewModel : ViewModel() {
         return responseResultLiveData
     }
 
-    suspend fun retrieveNameInfo(name: String) = withContext(Dispatchers.Default) {
-        val client = HttpClient(CIO) {
-            install(JsonFeature)
-        }
-        val ageEndpoint = "$AGE_ENDPOINT$name"
-        val genderEndpoint = "$GENDER_ENDPOINT$name"
-        val nationalityEndpoint = "$NATIONALITY_ENDPOINT$name"
+    fun retrieveNameInfo(name: String) {
+        viewModelScope.launch {
+            val client = HttpClient(CIO) {
+                install(JsonFeature)
+            }
+            val ageEndpoint = "$AGE_ENDPOINT$name"
+            val genderEndpoint = "$GENDER_ENDPOINT$name"
+            val nationalityEndpoint = "$NATIONALITY_ENDPOINT$name"
 
-        val ageResponse = async {
-            client.get<Age>(ageEndpoint)
-        }
-        val genderResponse = async {
-            client.get<Gender>(genderEndpoint)
-        }
-        val nationalityResponse = async {
-            client.get<Nationality>(nationalityEndpoint)
-        }
+            val ageResponse = async {
+                client.get<Age>(ageEndpoint)
+            }
+            val genderResponse = async {
+                client.get<Gender>(genderEndpoint)
+            }
+            val nationalityResponse = async {
+                client.get<Nationality>(nationalityEndpoint)
+            }
 
-        setResponseResult(
-            ResponseResult.parse(
-                ageResponse.await(),
-                genderResponse.await(),
-                nationalityResponse.await()
+            setResponseResult(
+                ResponseResult.parse(
+                    ageResponse.await(),
+                    genderResponse.await(),
+                    nationalityResponse.await()
+                )
             )
-        )
+        }
     }
 }
